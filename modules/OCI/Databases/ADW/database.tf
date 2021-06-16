d70b4d0e1fd34aceb26d66df3bb06bd7a2c555c6
williamdubberley <william.dubberley@sesamesoftware.com>
2021-06-16 10:28:25 -0500

    adw Module

---
+++ modules/OCI/Databases/ADW/database.tf
@@ -0,0 +1,44 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+
+resource "oci_database_autonomous_database" "test_autonomous_database" {
+    #Required
+    admin_password = var.autonomous_database_admin_password
+    compartment_id = var.compartment_ocid
+    cpu_core_count = var.autonomous_database_cpu_core_count
+    data_storage_size_in_tbs = var.autonomous_database_data_storage_size_in_tbs
+    db_name = var.autonomous_database_db_name
+    db_version = var.autonomous_database_db_version
+    data_safe_status = var.autonomous_database_data_safe_status
+    db_workload = var.autonomous_database_db_workload
+    display_name = var.autonomous_database_display_name
+    is_auto_scaling_enabled = var.autonomous_database_is_auto_scaling_enabled
+    license_model = var.autonomous_database_license_model   
+   # whitelisted_ips = var.autonomous_database_whitelisted_ips
+    count = var.adw_enabled == true ? 1 : 0
+
+}
+
+resource "random_string" "autonomous_data_warehouse_wallet_password" {
+  length  = 16
+  special = true
+}
+
+resource "oci_database_autonomous_database_wallet" "autonomous_data_warehouse_wallet" {
+  autonomous_database_id = oci_database_autonomous_database.test_autonomous_database[0].id
+  password               = random_string.autonomous_data_warehouse_wallet_password.result
+  base64_encode_content  = "true"
+}
+
+resource "local_file" "autonomous_data_warehouse_wallet_file" {
+  content_base64 = oci_database_autonomous_database_wallet.autonomous_data_warehouse_wallet.content
+  filename       = "${path.module}/autonomous_data_warehouse_wallet.zip"
+}
+
+output "autonomous_data_warehouse_wallet_password" {
+  value = random_string.autonomous_data_warehouse_wallet_password.result
+}
+output "autonomous_data_warehouse_wallet_Location" {
+  value = local_file.autonomous_data_warehouse_wallet_file.filename
+}
---
+++ modules/OCI/Databases/ADW/varibles.tf
@@ -0,0 +1,52 @@
+# Variables
+variable "adw_enabled" {
+  description = "whether to create ADW server"
+  default     = false
+  type        = bool
+}
+
+variable "autonomous_database_cpu_core_count" {
+  default = "1"
+}
+
+variable "autonomous_database_admin_password" {
+}
+
+variable "autonomous_database_db_name" {
+  default = "ADWDB"
+}
+
+variable "autonomous_database_display_name" {
+  default = "My ADW DB"
+}
+
+variable "autonomous_database_db_version" {
+  default = "19c"
+}
+
+variable "autonomous_database_is_auto_scaling_enabled" {
+  default = "false"
+}
+
+variable "autonomous_database_data_storage_size_in_tbs" {
+  default = "1"
+}
+
+variable "autonomous_database_db_workload" {
+  default = "DW"
+}
+
+variable "autonomous_database_license_model" {
+  default = "BRING_YOUR_OWN_LICENSE"
+}
+
+variable "autonomous_database_data_safe_status" {
+  default = "NOT_REGISTERED"
+}
+
+variable "compartment_ocid" {
+  default = ""
+}
+
+
+
